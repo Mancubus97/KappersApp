2df14c7d57373e11e5f03ae44bb64b6992def4e6
S. D. Maat <silvijndemaat1@gmail.com>
2026-02-19 03:38:42 +0100

    first commit

---
+++ Models/Customer.py
@@ -0,0 +1,10 @@
+class Customer:
+    def __init__(self, id, voornaam, achternaam, telefoon, email, opmerkingen, aangemaakt_op):
+        self.id = id
+        self.voornaam = voornaam
+        self.achternaam = achternaam
+        self.telefoon = telefoon
+        self.email = email
+        self.opmerkingen = opmerkingen
+        self.aangemaakt_op = aangemaakt_op
+        
